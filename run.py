9b29e0baac03ab4883e254aa743ee4ae16cc3804
Arturo Ávila Martínez <arturoavilamartinez9@gmail.com>
2025-02-10 10:22:13 -0600

    Cambios para server local

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import app
+import os
 
 if __name__ == '__main__':
+    # Se usa para levantar un server local
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
